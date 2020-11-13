cc583f2bfaea44d5874eaeae4d436bf87127810b
joelsprangers <68640121+joelsprangers@users.noreply.github.com>
2020-11-13 15:36:46 +0100

    Add files via upload

---
+++ speeltuin/script.js
@@ -0,0 +1,12 @@
+
+let name = 'joel';
+console.log(name);
+
+const three = 3;
+console.log(three % 2);
+
+let age = 48;
+let ageString = '48';
+
+console.log(typeof(age));
+console.log(typeof(ageString));
